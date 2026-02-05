92609cbc42f7cc5c493e72f23902ce2285cc4b70
Amarjeet-05 <ajbhardwaj2005@gmail.com>
2026-02-05 20:01:47 +0530

    Add files via upload

---
+++ 01_python/_1errorhandling.py
@@ -0,0 +1,25 @@
+n = input("Enter the no. : ")
+try:
+    for i in range(1, 11):
+        print(f"{n} x {i} = {int(n) * i}")
+except:
+    print("Invalid data")
+
+#Or we can also use exception as
+n = input("Enter the no. : ")
+try:
+    for i in range(1, 11):
+        print(f"{n} x {i} = {int(n) * i}")
+except Exception as a:#it automatically gives an error
+    print(a)
+
+#Specific type of errors
+try:
+    num = int(input("Enter a no. "))
+    a = [6, 4]
+    print(a[num])
+except ValueError:
+    print("It is not an integer")
+except IndexError:
+    print("Index Error")
+                         
