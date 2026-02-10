065720082edab6f91e4b45c18d79f1739a95ecc9
Amarjeet-05 <ajbhardwaj2005@gmail.com>
2026-02-11 00:21:41 +0530

    Add files via upload

---
+++ 06_SQL/06_Update_table.sql
@@ -0,0 +1,4 @@
+UPDATE std_data SET result = "pass" WHERE result = "await";
+UPDATE std_data SET age = age+1;
+
+SELECT * FROM std_data;
---
+++ 06_SQL/07_Delete_data.sql
@@ -0,0 +1,6 @@
+DELETE FROM std_data WHERE id = 1; -- it will delete first id.
+DELETE FROM std_data where age < 20;
+-- DELETE FROM std_data;  -- it will delete the content of the table
+
+-- DROP TABLE std_data; -- it will remove the table from the database.
+SELECT * FROM std_data;
