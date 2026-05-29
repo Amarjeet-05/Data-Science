c591095c0c59ee96485ea4ca135b39426b5b5715
Amarjeet <ajbhardwaj2005@gmail.com>
2026-05-29 17:33:12 +0530

    Add files via upload

---
+++ 08_Flask/_01Intro/_1intro.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")         
+def hello_world():
+    return "<p>Hello Aman!</p>"
+
+
+@app.route("/about")
+def about():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return "<p>This the contact page</p>"
+
+app.run(debug=True)
